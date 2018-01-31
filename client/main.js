c01af128a1c242a09cf97e9c790de56330e9d08a
walterguo <taguo92@gmail.com>
2018-01-30 21:30:00 -0800

    update title

--- client/main.js
+++ client/main.js
@@ -10,7 +10,7 @@ Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find({},{sort: {score:-1}}).fetch();
     let positionedPlayers = calculatePlayerPositions(players);
-    let title = 'Score Keep';
+    let title = 'Scoreboard';
     ReactDOM.render(<App title={title} players={positionedPlayers}/>, document.getElementById('app'));
   });
 });
