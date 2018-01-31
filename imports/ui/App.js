06af55bcb1afe6af31b67cdc3250b3932462263c
walterguo <taguo92@gmail.com>
2018-01-30 21:19:20 -0800

    update name

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <TitleBar title={this.props.title} subtitle={"Created by Andrew Mead"}/>
+        <TitleBar title={this.props.title} subtitle={"Created by Walter Guo"}/>
         <div className="wrapper">
            <PlayerList players={this.props.players}/>
            <AddPlayer/>
